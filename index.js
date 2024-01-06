5bc6afc46a545ed5509679969c44cff2e913e554
imkozin <ivan.kozin@hotmail.com>
2024-01-06 18:53:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const item = {
+  name: 'some_name',
+  body: 'some_description',
+  describe: function () {
+    // describe: () => {
+    console.log(`Item [${this.name}] contains: ${this.body}`)
+  },
+}
+item.describe()
+
+// const userData = JSON.parse(userInput)
+// const query = `SELECT * FROM users WHERE name = '${userData.name}' AND password = '${userData.password}'`
